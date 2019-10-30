29cc91774192fb5d993778355bb8b193ea97a245
Shanmugavel S <shanvel26@gmail.com>
2019-10-30 17:19:04 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+import './widgets/user_transactions.dart';
 
 void main() {
   runApp(ExpenseApp());
 }
 
 class ExpenseApp extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: "1",
-      title: 'Vegetables',
-      amount: 12,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "2",
-      title: 'Sweets',
-      amount: 24,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,94 +13,22 @@ class ExpenseApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Expense App"),
         ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text("CHART"),
-                elevation: 10,
-              ),
-            ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Title",
-                      ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Amount",
-                      ),
-                    ),
-                    FlatButton(
-                      child: Text('Add Transaction'),
-                      onPressed: () {},
-                      textColor: Colors.purple,
-                    ),
-                  ],
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text("CHART"),
+                  elevation: 10,
                 ),
               ),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 2,
-                            color: Colors.purple,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
-          ],
+              UserTransactions(),
+            ],
+          ),
         ),
       ),
     );
